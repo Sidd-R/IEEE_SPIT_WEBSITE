387201ede678c93fa0f8c4af1f338851cd877973
Samarth Gupta <samarthgupta1202@gmail.com>
2023-02-27 22:16:30 +0530

    Added link to the card component

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,20 +3,21 @@ import "../styles/BlogCard.css"
 
 export default function BlogCard(props) {
 
-    console.log(props.link);
-
     return (
-        <div className="card">
-            <div >
-                <a href={props.link} target="_blank"><img src={props.img} className="card_image" /></a>
-            </div>
-            <div className="card__copy">
-                <h1>{props.title}</h1>
-                <h2 className="date">{props.date}</h2>
-                <p>
-                    {props.content}
-                </p>
+        <div className="BlogCardContainer">
+            <div className="card">
+                <div>
+                    <a href={props.link} target="_blank"><img src={props.img} className="card_image" /></a>
+                </div>
+                <div className="card__copy">
+                    <h1>{props.title}</h1>
+                    <h2 className="date">{props.date}</h2>
+                    <p>
+                        {props.content}
+                    </p>
+                </div>
             </div>
         </div>
+
     )
 }
--- src/components/Blog_description.js
+++ src/components/Blog_description.js
@@ -1,10 +1,11 @@
 import electric_car from "../images/electric_car.jpg"
 import router from "../images/router.png"
+import api from "../images/api.png"
 
 
 const Blog = [{
     img: electric_car,
-    title: "The Electric Vehical",
+    title: "The EVs",
     date: "Nov 7, 2022",
     content: "It is 2022! EVs have almost come to parity with the normal petrol fueled car. The range of the cars have also seen an increase, with scientists cramming more and more energy into the batteries. Click here to read this blog!",
     link: "https://medium.com/@ieee_spit/the-electric-vehicle-isnt-enough-b1d8d86535f5"
@@ -14,6 +15,13 @@ const Blog = [{
     date: "Nov 21, 2022",
     content: "You want to surf the internet, maybe watch web series, stream anime or just catch on with the world on social media, you need an internet connection provided by an Internet Service Provider (ISP). Click here to read more about routers and why we use them!",
     link: "https://medium.com/@ieee_spit/what-is-a-router-2c785f8fd2d4"
+}, {
+    img: api,
+    title: "What are APIs?",
+    date: "Dec 30, 2022",
+    content: "Suppose you want to know the weather of your location from the weather app. How will you get the weather until you are somehow connected to the weather bureau. APIs make that connection for you. Click here to read more about APIs and why it is used !",
+    link: "https://medium.com/@ieee_spit/what-are-apis-bd20498e2828"
+
 }]
 
 export default Blog
