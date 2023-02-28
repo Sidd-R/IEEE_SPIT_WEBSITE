984834ea131d267ca6742a5f8dcd29a3ee34dace
navneetnair7 <navneetnair95@gmail.com>
2023-02-28 17:35:00 +0530

    Updated Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,19 @@
 import BlogCard from "../components/BlogCard.jsx"
 import Blog from "../components/Blog_description.js"
+import '../styles/Home.css'
 
 export const Home = () => {
   return (
-    Blog.map(blog => {
-
-      return (
-        <BlogCard img={blog.img}
-          title={blog.title}
-          date={blog.date}
-          content={blog.content}
-          link={blog.link}
-
-        />
-      )
-    })
+    <div className="blog_container">
+      {Blog.map(blog => {
+          return (<BlogCard img={blog.img}
+            title={blog.title}
+            date={blog.date}
+            content={blog.content}
+            link={blog.link}
+          />)}
+      )}
+    </div>
   )
 }
 
