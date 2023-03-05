117f2f385b9395d6421a8233034ff93d787b67d1
Sidd-R <siddhant.rao@spit.ac.in>
2023-03-05 22:02:26 +0530

    added main animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,22 @@
 import BlogCard from "../components/BlogCard.jsx"
 import Blog from "../components/Blog_description.js"
+import MainAnimation from "../components/MainAnimation.js"
 import '../styles/Home.css'
 
 export const Home = () => {
   return (
-    <div className="blog_container">
-      {Blog.map(blog => {
-          return (<BlogCard img={blog.img}
-            title={blog.title}
-            date={blog.date}
-            content={blog.content}
-            link={blog.link}
-          />)}
-      )}
+    <div>
+      <MainAnimation/>
+      <div className="blog_container">
+        {Blog.map(blog => {
+            return (<BlogCard img={blog.img}
+              title={blog.title}
+              date={blog.date}
+              content={blog.content}
+              link={blog.link}
+            />)}
+        )}
+      </div>
     </div>
   )
 }
