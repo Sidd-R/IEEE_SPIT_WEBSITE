1e41f9e277c4a9aee15b737f3421f6e3cc026498
navneetnair7 <navneetnair95@gmail.com>
2023-03-05 22:55:06 +0530

    Events Page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,5 +1,26 @@
+import TeamCard from "../components/TeamCard"
+import '../styles/Team.css'
+import team from "../components/Team_description"
+import bgvid from '../images/video-bg-light.mp4'
+
 export const Team = () => {
   return (
-    <div>Team</div>
+    <div className="team_container">
+        <div className="team_title">
+          Our Team
+        </div>
+        <div className="team_cards">
+          {team.map(member => {
+            return(
+              <TeamCard 
+                name = {member.name}
+                post = {member.post}
+                image = {member.image}
+              />
+            )
+            })
+          }
+        </div>
+      </div>
   )
 }
---
+++ src/components/Team_description.js
@@ -0,0 +1,75 @@
+import kevin from '../images/team/kevin.jpeg'
+import atharva from '../images/team/atharva.jpeg'
+// import ayushi from '../images/team/ayushi.jpg'
+import samarth from '../images/team/samarth.jpg'
+import navneet from '../images/team/navneet.jpeg'
+import siddhant from '../images/team/siddhant.jpg'
+import shubh from '../images/team/shubh.jpg'
+import ayushi2 from '../images/team/ayushi2.jpeg'
+import dewyani from '../images/team/dewyani.jpeg'
+import sridhar from '../images/team/sridhar.JPG'
+import gauri from '../images/team/gauri.jpeg'
+import devanshi from '../images/team/devanshi.jpg'
+import kevind from '../images/team/kevind.jpg'
+import malay from '../images/team/malay.jpg'
+import jaivik from '../images/team/jaivik.jpeg'
+
+const team = [{
+    name: "Kevin Xavier",
+    post: "Chairperson",
+    image: kevin
+}, {
+    name: "Atharva Tadge",
+    post: "Vice Chairperson",
+    image: atharva
+}, {
+    name: "Ayushi Naik",
+    post: "Secretary",
+    image: ayushi2
+}, {
+    name: "Samarth Gupta",
+    post: "Technical Head",
+    image: samarth
+}, {
+    name: "Navneet Nair",
+    post: "Technical Subordinate",
+    image: navneet
+}, {
+    name: "Siddhant Rao",
+    post: "Technical Subordinate",
+    image: siddhant
+}, {
+    name: "Malay Phadke",
+    post: "Technical Subordinate",
+    image: malay
+}, {
+    name: "Shubh Gupta",
+    post: "Special Projects Head",
+    image: shubh
+}, {
+    name: "Jaivik Parekh",
+    post: "Special Projects Head",
+    image: jaivik
+}, {
+    name: "Sridhar Sundar",
+    post: "Event Head",
+    image: sridhar
+}, {
+    name: "Dewyani",
+    post: "PR Head",
+    image: dewyani
+}, {
+    name: "Gauri",
+    post: "PR Head",
+    image: gauri
+}, {
+    name: "Devanshi Padhy",
+    post: "Marketing Head",
+    image: devanshi
+}, {
+    name: "Keivin Doshi",
+    post: "Finance Head",
+    image: kevind
+}]
+
+export default team
