03e3d3e7ccc3dd80b831ef4a333a931598f6a871
SoulDev07 <mayankravariya@gmail.com>
2023-11-24 15:29:17 +0530

    Group event images

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,6 +1,6 @@
 // import React, { useState } from "react";
 import '../styles/Events.css';
-import { events, events2023 } from '../components/Event_description';
+import { events22, events23 } from '../components/Event_description';
 import EventCard from '../components/EventCard';
 import Our from '../components/Our';
 
@@ -9,8 +9,8 @@ function Events() {
     <>
       <Our x={'Events'} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3   md:px-28 px-8 mt-10 mx-auto place-items-center">
-        {events2023.map((e,i) => {
-          return <EventCard name={e.name} desc={e.desc} image={e.image} key={i}/>;
+        {events23.map((e, i) => {
+          return <EventCard name={e.name} desc={e.desc} image={e.image} key={i} />;
         })}
       </div>
       <div className=" font-sans text-3xl font-normal mx-8 mb-1 sm:mx-28 mt-6 text-gray-100">
@@ -20,8 +20,8 @@ function Events() {
       <hr className=" bg-black h-0.5   mx-5 sm:mx-24 " />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3   md:px-28 px-8 mt-10 mx-auto place-items-center">
-        {events.map((e,i) => {
-          return <EventCard name={e.name} desc={e.desc} image={e.image} key={i}/>;
+        {events22.map((e, i) => {
+          return <EventCard name={e.name} desc={e.desc} image={e.image} key={i} />;
         })}
       </div>
     </>
--- src/components/Event_description.js
+++ src/components/Event_description.js
@@ -1,72 +1,73 @@
-import audience from '../images/audience.png';
-import speaker2 from '../images/speaker2.jpeg';
-import techbrawl1 from '../images/techbrawl1.jpeg';
-import placement1 from '../images/placement1.jpeg';
-import codehunt from '../images/codehunt.jpg';
-import IC from '../images/IC.jpg';
-import gsoc from '../images/gsoc_webinar.png';
-import peripherithon from '../images/Peripherithon.jpg';
-import aarambh23 from '../images/aarambh23.jpg';
-import ieeeday23 from '../images/ieeeday23.jpg';
-import blog23 from '../images/blog23.jpg';
+function importAll(r) {
+  let images = {};
+  r.keys().forEach(item => {
+    images[item.replace('./', '').replace(/\.(jpg|jpeg|png)$/, '')] = r(item);
+  });
+  return images;
+}
 
-export const events2023 = [
+const images22 = importAll(require.context('../images/events22', false, /\.(jpeg|jpg|png)$/));
+const images23 = importAll(require.context('../images/events23', false, /\.(jpeg|jpg|png)$/));
+
+const events23 = [
   {
     name: 'Blog Writing 2023',
     desc: 'Circular Economy X Blog Writing Competition',
-    image: blog23,
+    image: images23['blog'],
   },
   {
     name: 'IEEE Day 2023',
     desc: 'Celebration of IEEE Day with our Honoured Guests',
-    image: ieeeday23,
+    image: images23['ieeeday'],
   },
   {
     name: 'Aarambh 2023',
     desc: 'An Ice breaker Session for our Freshers',
-    image: aarambh23,
+    image: images23['aarambh'],
   },
 ];
 
-export const events = [
+const events22 = [
   {
     name: 'Peripherathon 2023',
     desc: 'A AI powered IoT hackathon',
-    image: peripherithon,
+    image: images22['peripherithon'],
   },
   {
     name: 'codeHunt 2023',
     desc: 'A mix of treasure hunt and a CP contest along with the guidance of ICPC finalist Vivek Gupta',
-    image: codehunt,
+    image: images22['codehunt'],
   },
   {
     name: 'Innovation Cup 2023',
     desc: 'A hardware hackathon',
-    image: IC,
+    image: images22['IC'],
   },
   {
     name: 'GSOC Webinar 2023',
     desc: 'A webinar on how to get selected for GSOC',
-    image: gsoc,
+    image: images22['gsoc_webinar'],
   },
   {
     name: 'Tackling Placements 2023',
     desc: "A seminar by our college's alumni about placements and higher studies",
-    image: placement1,
+    image: images22['placement'],
   },
   {
     name: 'Webinar on Blockchain',
     desc: 'A webinar by instustry experts to get insights on the current usage of blockchain',
-    image: speaker2,
+    image: images22['blockchain_speaker2'],
   },
   {
     name: 'Aarambh 2022',
     desc: 'A fun ice breaker session for FEs',
-    image: audience,
+    image: images22['aarambh'],
   },
   {
     name: 'Techbrawl 2022',
     desc: 'A tech competition for FEs',
-    image: techbrawl1,
+    image: images22['techbrawl1'],
   },
 ];
+
+export { events22, events23 };
